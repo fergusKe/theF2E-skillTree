c2340ad44ee02b6fd6485ea7fcb936aa912098ec
Fergus <fergus.jingtai@gmail.com>
2018-08-06 11:49:43 +0800

    add layout

---
+++ src/components/Dialog.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Dialog = ({ label, content }) => (
+  <div>
+    <h1>{label}</h1>
+    <span>{content}</span>
+  </div>
+);
+
+export default Dialog;
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import SkillTree from '../../components/SkillTree';
+import Dialog from '../../components/Dialog';
 
 class Home extends Component {
+  state = {
+    info: null
+  };
+  showInfo = (skill) => {
+    this.setState({
+      info: skill
+    });
+  };
   render() {
+    const { info } = this.state;
+    console.log('info = ', info);
     return (
-      <div id="app">
-        App
-      </div>
+      <Router>
+        <div>
+          <SkillTree showInfo={this.showInfo} />
+          {info && <Dialog {...info} />}
+        </div>
+      </Router>
     );
   }
 }
